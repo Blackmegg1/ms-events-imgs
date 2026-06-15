25b223b0a98db9fedf6eef8ca4fd6dc25860f049
Blackmegg1 <1141066795@qq.com>
2026-06-15 16:24:39 +0800

    feat: 修改版本号

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -110,7 +110,7 @@ const Login: React.FC = () => {
             />
             <div className={styles.content}>
                 <div className={styles.login}>
-                    <div className={styles.title}>透明地质数据处理系统 v2.4 </div>
+                    <div className={styles.title}>透明地质数据处理系统 v2.5</div>
                     <LoginForm
                         subTitle=" "
                         initialValues={{
